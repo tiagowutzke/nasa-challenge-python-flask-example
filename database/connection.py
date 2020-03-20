399f7006891368b52a575d2a5e230b8710feb218
tiagowutzke <tiago.oliveira@contaazul.com>
2020-03-20 11:40:32 -0300

    Adjust buildpacks on Heroku

--- database/connection.py
+++ database/connection.py
@@ -16,7 +16,7 @@ class Connection:
             host='ec2-52-200-119-0.compute-1.amazonaws.com',
             database='d2vb1674stun3q',
             user='nprdjtrodglaic',                                          
-            password='4d670d30987de1f8704bdd8ec2470396638f25f27ebce0840f2a762fd2c40c5c
+            password='4d670d30987de1f8704bdd8ec2470396638f25f27ebce0840f2a762fd2c40c5c'
 '
         )
         return conn, conn.cursor()
