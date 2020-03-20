6789b9c810b01a8e94dc49657bc82a690e8b01ff
tiagowutzke <tiago.oliveira@contaazul.com>
2020-03-20 19:40:48 -0300

    select options logic

--- app.py
+++ app.py
@@ -189,6 +189,6 @@ def api_score():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
     adapter.close()
 
--- static/js/functions.js
+++ static/js/functions.js
@@ -21,16 +21,69 @@ function setInputValue(input_id, option_selected) {
     document.getElementById(input_id).setAttribute('value', option_selected.value);
 }
 
+// Listen changes in <select>
+$(document).ready(function() {
+    var previous_value;
+
+    $("select").on('focus', function () {
+        // Store the current value on focus and on change
+        previous_value = this.value;
+    }).change(function() {
+        should_enable_submit('form-control');
+        dropOthersValues(this, previous_value);
+    });
+})();
+
+
 // Check duplicate order values
-function dropOthersValues() {
-    var selects = document.getElementsByTagName('select');
-    var values = [];
-    for(i=0;i<selects.length;i++) {
-        var select = selects[i];
-        if(values.indexOf(select.value)>-1 && select.value > 0) {
-            alert('Verifique a seguinte ordem duplicada: '+ select.value); break;
-        }
-        else
-            values.push(select.value);
+function dropOthersValues(select, previous_value) {
+    actual_value = getOption(select.id)
+
+    if(actual_value){
+        removeOption(select.id, actual_value);
+        addOption(select.id, actual_value, previous_value);
     }
 }
+
+function getOption(select_id){
+    var select = document.getElementById(select_id);
+    if(select.options.length > 0) {
+        return select.value;
+    }
+    return false;
+}
+
+
+function addOption(select_id, actual_value, previous_value){
+    var selects = document.getElementsByTagName('select');
+    for(i=0; i<selects.length; i++) {
+        if(selects[i].id === select_id) continue;
+
+        for(k=0; k<selects[i].length; k++){
+            if(selects[i].options[k].value === previous_value)
+                return false;
+        }
+
+        // create new option element
+        var opt = document.createElement('option');
+        // create text node to add to option element (opt)
+        opt.appendChild( document.createTextNode(previous_value) );
+        // set value property of opt
+        opt.value = previous_value;
+        // add opt to end of select box (sel)
+        selects[i].appendChild(opt);
+
+    }
+}
+
+function removeOption(select_id, actual_value){
+    var selects = document.getElementsByTagName('select');
+    for(i=0; i<selects.length; i++) {
+        if(selects[i].id === select_id) continue;
+        for(k=1; k<selects[i].length; k++){
+            if(selects[i].options[k].value === actual_value)
+                selects[i].options[k] = null;
+        }
+    }
+}
+
