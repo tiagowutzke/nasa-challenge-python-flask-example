db16e428b40b7901d5515ce8c1dace9fb52520e0
tiagowutzke <tiago.oliveira@contaazul.com>
2020-03-20 11:30:03 -0300

    Adjust buildpacks on Heroku

--- database/connection.py
+++ database/connection.py
@@ -13,10 +13,11 @@ class Connection:
     @staticmethod
     def _get_conn():
         conn = psycopg2.connect(
-            host='localhost',
-            database='reuse_db',
-            user='local',
-            password='1234'
+            host='ec2-52-200-119-0.compute-1.amazonaws.com',
+            database='d2vb1674stun3q',
+            user='nprdjtrodglaic',                                          
+            password='4d670d30987de1f8704bdd8ec2470396638f25f27ebce0840f2a762fd2c40c5c
+'
         )
         return conn, conn.cursor()
 
